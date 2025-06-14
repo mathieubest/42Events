3680f84552cc18b646900ef364e3dfbdc9ebcbf5
shiftwave <dev@shiftwave.me>
2025-06-14 19:42:15 +0200

    feat: add campus retrieval id func

--- index.ts
+++ index.ts
@@ -10,6 +10,37 @@ interface TokenResponse {
 	secret_valid_until: number
 }
 
+interface CampusResponse {
+	id: number,
+	name: string,
+	time_zone: string,
+	language: {
+		id: number,
+		name: string,
+		identifier: string,
+	},
+	users_count: number,
+	vogsphere_id: number,
+	country: string,
+	address: string,
+	zip: string,
+	city: string,
+	website: string,
+	facebook: string,
+	twitter: string,
+	active: boolean,
+	public: boolean,
+	email_extension: string,
+	default_hidden_phone: boolean,
+	endpoint: {
+		id: number,
+		url: string,
+		description: string,
+		created_at: string,
+		updated_at: string
+	}
+}
+
 async function getToken(): Promise<string | undefined> {
 	const clientId = process.env.CLIENT_ID;
 	const clientSecret = process.env.CLIENT_SECRET;
@@ -33,12 +64,34 @@ async function getToken(): Promise<string | undefined> {
 		});
 }
 
+async function getCampusId(token: string, campus_name: string): Promise<number | undefined> {
+	return axios.get(`https://api.intra.42.fr/v2/campus/${campus_name}`, {
+		headers: {
+			Authorization: `Bearer ${token}`
+		}
+	})
+		.then((response: AxiosResponse<CampusResponse>) => {
+			return response.data.id;
+		})
+		.catch(error => {
+			console.error("Error retrieving campus ID:", error);
+			return undefined;
+		}
+		);
+}
+
 async function main() {
 	const token = await getToken();
 	if (!token) {
 		console.error("Token not retrieved, check your credentials.");
+		return;
 	} else {
 		console.log("Token successfully retrieved:", token);
+	}
+	const campusId = await getCampusId(token, 'paris');
+	if (!campusId) {
+		console.error("Campus ID not retrieved, check your token or campus name.");
+		return;
 	}
 }
 
